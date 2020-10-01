a64c7486eb9126a83c8b03d5c4c7967b99c0ef7c
Git2sep <66361624+Git2sep@users.noreply.github.com>
2020-10-01 06:50:15 +0530

    Update EvenOdd.c

--- EvenOdd.c
+++ EvenOdd.c
@@ -3,19 +3,19 @@
 int main()
 {
 	int number;  
-	printf("Please enter an integer: "); //Asking user for the input as a number//
+	printf("\n\t\tCheck Wheather an integer is EVEN (OR) ODD \t\t\n");
+	printf("Please enter an integer: \n"); //Asking user for the input as a number//
 	scanf("%d", &number);
 	
-	{if(number % 2 ==0)    //Checking if the number is Even or Odd by dividing the remainder by 2//
+	if(number % 2 ==0) 
+	{						//Checking if the number is Even or Odd by dividing the remainder by 2//
 		printf("%d is even\n", number); //If remainder is 0, then the integer is Even//
 	}
-	else {
+	else 
+	{
 		printf("%d is odd\n", number);  //If remainder is not 0, then its Odd//
 	}
 	
 	return 0;
-	
-	
-	
 	
 }
